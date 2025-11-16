7dc65c60e13d9422f0a7773c62e2f889333c8182
Christabel-Akpene <dc.akpene@gmail.com>
2025-11-16 13:44:25 +0000

    add new directory

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,16 @@
+var teacher3 = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: false,
+    location: "Accra",
+    contract: false,
+};
+console.log(teacher3);
+var director1 = {
+    firstName: "John",
+    lastName: "Doe",
+    location: "London",
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
