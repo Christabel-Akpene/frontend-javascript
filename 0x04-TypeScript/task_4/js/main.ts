72aa9d7599d9a9cbe8dabe2cb12f7b326ed0b841
Christabel-Akpene <dc.akpene@gmail.com>
2025-11-16 20:41:36 +0000

    changes

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,11 +4,16 @@
 /// <reference path="subjects/Java.ts" />
 /// <reference path="subjects/React.ts" />
 
-const cpp = new Subjects.Cpp();
-const java = new Subjects.Java();
-const react = new Subjects.React();
+import Cpp = Subjects.Cpp;
+import Java = Subjects.Java;
+import React = Subjects.React;
+import Teacher = Subjects.Teacher;
 
-const cTeacher: Subjects.Teacher = {
+const cpp = new Cpp();
+const java = new Java();
+const react = new React();
+
+const cTeacher: Teacher = {
   firstName: "John",
   lastName: "Doe",
   experienceTeachingC: 10
